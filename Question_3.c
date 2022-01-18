b03ca3ee0cf766c66078f404f01a8db7229c4345
Oscarbre <92889582+Oscarbre@users.noreply.github.com>
2022-01-18 11:32:01 +0100

    Update Question_3.c

--- Question_3.c
+++ Question_3.c
@@ -1,4 +1,4 @@
-// Exit : Ajout d'une fonctionnalité pour sorti du shell
+// Exit : Ajout d'une fonctionnalité pour sortir du shell
 
 # include <string.h>
 # include <stdlib.h>
@@ -17,17 +17,17 @@ void main(){
 	while(continuer){
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		read(STDIN_FILENO, stringIn,64);
-		if ((strncmp(stringIn, "fortune", strlen("fortune"))==0)){				// Attention, en procédant comme ceci, seule la commande fortune peut être 
-			pid_t pid=fork();								// executée. On change la manière de procéder par la suite pour executer 
-			if (pid==0){									// toutes les commandes
-				execlp("fortune","f",NULL);
+		if ((strncmp(stringIn, "fortune", strlen("fortune"))==0)){			// Remarque : en procédant comme ceci, seule la commande fortune peut être 
+			pid_t pid=fork();							// executée  (on entre dans la boucle seulement si "fortune" est entrée	 
+			if (pid==0){								// dans le shell). On change la manière de procéder par la suite pour executer
+				execlp("fortune","f",NULL);					// toutes les commandes.
 			}
 			else{
 				wait(NULL);
 			}	
 			
-		}else if ((strncmp(stringIn, "exit", strlen("exit"))==0)){				// on ajoute les 3 lignes qui suivent et on modifie la condition du while
-			write(STDOUT_FILENO,"Bye bye...\n$\n", strlen("Bye bye...\n$\n"));		// ce qui permet de sortir de la boucle si on écrit "exit"
+		}else if ((strncmp(stringIn, "exit", strlen("exit"))==0)){			// on ajoute les 3 lignes qui suivent et on modifie la condition du while
+			write(STDOUT_FILENO,"Bye bye...\n$\n", strlen("Bye bye...\n$\n"));	// ce qui permet de sortir de la boucle si on écrit "exit"
 			continuer=0;
 		}
 	}
